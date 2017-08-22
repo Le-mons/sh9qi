e568d7803b53b8218e7ed9b62e78d754c545f753
wang <138464412@qq.com>
2017-08-22 22:10:29 +0800

    项目搭建

---
+++ src/components/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    首页
+    <mt-button type="danger" size="large">哈哈</mt-button>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    data(){
+      return {}
+    },
+    created(){
+      this.$ajax.get('getlunbo')
+          .then(res=>{
+        console.log(res);
+    })
+    .catch(err=>{
+        console.log(err);
+    })
+    }
+  }
+</script>
+<style>
+
+</style>
